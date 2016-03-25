7aa581c6536cc839ebca9b63fca1bf0fe0882151
Daniel Quackenbush <thequack45@gmail.com>
2016-03-24 21:09:29 -0400

    Added algorithm functionality

--- GreedyAlgorithmChangeMaking/Program.cs
+++ GreedyAlgorithmChangeMaking/Program.cs
@@ -10,6 +10,47 @@ namespace GreedyAlgorithmChangeMaking
     {
         static void Main(string[] args)
         {
+            List<decimal> denominationsList = new List<decimal>()
+            {
+                0.25m,
+                0.1m,
+                0.05m,
+                0.01m,
+            };
+            List<int> denominationsCountList = new List<int>()
+            {
+                0,
+                0,
+                0,
+                0,
+            };
+            decimal total;
+
+            int denominationsListIndex = 0;
+
+            Console.WriteLine("What amount of money to make change for?");
+            total = decimal.Parse(Console.ReadLine());
+
+            do
+            {
+                if (total >= denominationsList[denominationsListIndex])
+                {
+                    total -= denominationsList[denominationsListIndex];
+                    denominationsCountList[denominationsListIndex]++;
+                }
+                else
+                {
+                    denominationsListIndex++;
+                }
+            } while (total > 0);
+
+            Console.WriteLine("Coins used: ");
+            for (int i = 0; i < denominationsList.Count; i++)
+            {
+                Console.WriteLine(denominationsList[i] + " Coins: " + denominationsCountList[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
